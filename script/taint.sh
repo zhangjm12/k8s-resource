699f716084ee7b10d28d76f56020054afdc59927
root <root@dev00.novalocal>
2017-04-04 22:37:34 +0800

    update v1.6

--- script/taint.sh
+++ script/taint.sh
@@ -1,2 +1,2 @@
-kubectl taint nodes 172.31.0.11 dedicated=master:NoSchedule
-kubectl label nodes 172.31.0.11 kubeadm.alpha.kubernetes.io/role=master
+kubectl taint nodes 172.31.0.23 dedicated=master:NoSchedule
+kubectl label nodes 172.31.0.23 kubeadm.alpha.kubernetes.io/role=master
